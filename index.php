612a374c1a3381a7ff0804612e8f94d0c03095e1
Bárbara <barbara.tribino@gmail.com>
2018-09-19 21:34:31 -0300

    alterações

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
     <link rel="stylesheet" type="text/css" href="style.css">
     <form class="" method="post">
       <h1>Gerador de Imagens do Bill Murray</h1>
-      <input type="number" name="largura" value="<?php echo($_POST['largura']) ?>" placeholder="Largura" max="800">
-      <input type="number" name="altura" value="<?php echo($_POST['altura']) ?>" placeholder="Altura" max="800">
+      <input type="number" name="largura" value="<?php echo(isset($_POST['largura'])? $_POST['largura'] : "") ?>" placeholder="Largura" max="800">
+      <input type="number" name="altura" value="<?php echo(isset($_POST['altura'])? $_POST['altura'] : "") ?>" placeholder="Altura" max="800">
       <select name="categoria">
         <option value="calm">Calmo</option>
         <option value="gray">Cinza</option>
@@ -24,9 +24,15 @@
     ?>
       <img src="<?php echo "https://www.fillmurray.com/{$_POST['largura']}/{$_POST['altura']}"; ?>" alt="">
     <?php
-      else:
+      elseif (isset($_POST['categoria']) && $_POST['categoria'] == "gray"):
     ?>
       <img src="<?php echo "https://www.fillmurray.com/g/{$_POST['largura']}/{$_POST['altura']}"; ?>" alt="">
+    <?php
+      else:
+    ?>
+    <br/>
+    <p>Bem vindo(a) ao gerador de imagens do Bill Murray</p>
+    <p>Por favor, insira as dimensões da imagem e clique em enviar.</p>
     <?php
       endif
     ?>
